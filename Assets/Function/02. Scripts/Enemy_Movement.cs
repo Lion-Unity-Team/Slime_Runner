151f09676037020b0030c58b01049cca26826f7f
ZeroDimen <rlaehdrjs951@naver.com>
2025-08-10 20:56:45 +0900

    Add Slime Asset
    Player_Slime.cs 수정
    플레이어 점수와 슬라임 점수 비교를 위한 playerHp static 선언
    슬라임 파괴 주석처리
    
    Enemy_Movement.cs 수정
    Attack 및 Dead 애니메이션 조건 추가
    슬라임 파괴 추가

--- Assets/Function/02. Scripts/Enemy_Movement.cs
+++ Assets/Function/02. Scripts/Enemy_Movement.cs
@@ -1,19 +1,70 @@
+using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
+    public Animator animator;
+    public TextMeshProUGUI enemyText;
+    private RaycastHit2D hit;
+    
+    
+    public float rayDist;
     public float speed;
+    private int enemyScore;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        if(collision.CompareTag("Ground"))
-        {
-            Destroy(gameObject);
-        }
-        
+        enemyScore = int.Parse(enemyText.text);
     }
+
     private void Update()
     {
-        transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;    //y방향으로 -1씩 움직이는 명령어 * 속도 * 시간당프레임속도
+
+        Move();
+        
+        hit = Physics2D.Raycast(transform.position, Vector2.down, rayDist); 
+        Debug.DrawRay(transform.position, Vector2.down * rayDist, Color.red);
+
+        AttackCheak(); 
+    }
+
+    private void Move()
+    {
+        // y방향으로 -1씩 움직이는 명령어 * 속도 * 시간당프레임속도
+        transform.position += new Vector3(0, -1, 0) * (speed * Time.deltaTime);   
+    }
+
+    private void AttackCheak() // 현제 슬라임이 플레이어 보다 점수가 높다면 공격
+    {
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                if (PlayerSlime.playerHp <= enemyScore)
+                {
+                    animator.Play("Attack");
+                }
+            }
+        }
+    }
+    
+    private void DestroySlime()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            DestroySlime();
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            speed = 0;
+            animator.Play("Dead");
+        }
     }
 }
