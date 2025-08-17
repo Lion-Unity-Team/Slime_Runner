28ecdd9dffb2bddcf32febe05caf80b0cf035953
Gang <kingrkddn@gmail.com>
2025-08-17 19:14:43 +0900

    엔딩2. 2분 이상 생존

---
+++ Assets/Function/02. Scripts/TIMER.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayTimeUI : MonoBehaviour
+{
+    public GameObject player;
+    public TMP_Text playTimeText;  // 연결할 TMP 텍스트
+    public GameObject UI1;    // 플레이타임을 측정할 UI
+    public GameObject UI2;    // 플레이타임을 측정할 UI
+    public GameObject UI3;    // 플레이타임을 측정할 UI
+    public GameObject UI4;    // 플레이타임을 측정할 UI
+
+    private float playTime; // 누적 시간
+    private Animator _anime;
+
+    private void Start()
+    {
+        _anime = player.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // targetUI가 비활성화되어 있을 때만 시간 증가
+        if (!UI1.activeSelf && !UI2.activeSelf && !UI3.activeSelf)
+        {
+            playTime += Time.deltaTime;
+        }
+
+
+        // 시, 분, 초 변환
+        int minutes = (int)((playTime % 3600) / 60);
+        int seconds = (int)(playTime % 60);
+
+        // UI에 표시
+        playTimeText.text = $"{minutes:00}:{seconds:00}";
+        if(minutes == 2)
+        {
+            UI4.SetActive(false);
+            FindObjectOfType<GameStartManager>().EndGame();
+            FindObjectOfType<GameOverManager>().Score();
+            _anime.speed = 0f;
+        }
+    }
+}
